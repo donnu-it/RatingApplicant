d9aa2c98392c88c6020a45711742661c10dd833d
Yulia Afanasenko <yulia.a.a@outlook.com>
2016-05-12 17:19:41 +0300

    Round

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,20 @@
+jQuery('.form-control').keypress(function(event) {
+    // Backspace, tab, enter, end, home, left, right
+    // We don't support the del key in Opera because del == . == 46.
+    var controlKeys = [8, 9, 13, 35, 36, 37, 39];
+    // IE doesn't support indexOf
+    var isControlKey = controlKeys.join(",").match(new RegExp(event.which));
+    // Some browsers just don't raise events for control keys. Easy.
+    // e.g. Safari backspace.
+    if (!event.which || // Control keys in most browsers. e.g. Firefox tab is 0
+        (49 <= event.which && event.which <= 57) || // Always 1 through 9
+        (48 == event.which && $(this).attr("value")) || // No 0 first digit
+        isControlKey) { // Opera assigns values for control keys.
+        return;
+    } else {
+        event.preventDefault();
+    }
+});
 $(".subjectcheck").click(function() {
     var selected_value = [];
     $(".subjectcheck:checked").each(function(){
@@ -37,12 +54,6 @@ $('input:checkbox.subjectcheck').click(function() {
 
 $('input.form-control').on('change keyup', function() {
     var value = $(this).val();
-    if(value < 100 || value > 200 ) {
-        $(this).css("border-color", "red");
-    }
-    else {
-        $(this).css("border-color", "#ccc");
-     }
     $('table').find('.td-info-rating .rating_value').text("");
     $('table').find('.td-info-rating .rating_text').text("");
 });
@@ -58,7 +69,9 @@ $('#btnRating').click(function()  {
         var selected_value = 0;
         var i = 0;
         var flag = 0;
-
+        Number.prototype.round = function() {
+            return Math.round(this * 100) / 100;
+        }
 
         // отримати коефіцієнти предметів для кожного напряму підготоввки
         $(this).find('.td-info-ball .weight').each(function() {
@@ -78,8 +91,9 @@ $('#btnRating').click(function()  {
                 var index = $(this).index('.subjectcheck');
                 if(subject_requere == selected_value) {
                     sum += $('#form-control' + index).val() * koef_value[i];
-                    if($('#form-control' + index).val() > 200 || $('#form-control' + index).val() < 100)
+                    if($('#form-control' + index).val() > 200 || $('#form-control' + index).val() < 100) {
                         flag = 1;
+                    }
                     if ($('#form-control' + index).val() < min_value[i]) {
                         flag = 1;
                     }
@@ -102,13 +116,12 @@ $('#btnRating').click(function()  {
         // розрахувати по найбільшому балові із необов"якових предметів конкурсний бал та отримати загальний конкурсний бал по напряму
         subject_no_requere_value.sort().reverse();
         sum += subject_no_requere_value[0] * koef_value[2];
-        console.log("flag", flag);
+
         if( flag == 0 ) {
-            $(this).find('.td-info-rating .rating_value').text(sum);
+            $(this).find('.td-info-rating .rating_value').text(sum.round());
             $(this).find('.td-info-rating .rating_text').text("Конкурсний бал: ");
         }
         flag = 0;
-        console.log("tr");
     });
 });
 
