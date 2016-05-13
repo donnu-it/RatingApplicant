c3b6096d9229ac1417a60a1e9b2b55fb68890a00
Yulia Afanasenko <yulia.a.a@outlook.com>
2016-05-13 18:39:54 +0300

    vso version

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var config = {}
 
-config.host = process.env.HOST || "***";
-config.authKey = process.env.AUTH_KEY || "***";
+config.host = process.env.HOST || "https://ratingapplicantsdb.documents.azure.com:443/";
+config.authKey = process.env.AUTH_KEY || "QK1NgeIB5oGyM8iCTdsydciu3wmVBgmDpBggMJOfsimF5yFuRZCHhnI2s1pR3qQkFLKtB2aq3CQsCLekJapHBw==";
 config.databaseId = "ratingapplicantsdb";
 config.collectionId = "specializations";
 
