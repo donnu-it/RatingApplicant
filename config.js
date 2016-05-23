3ecfbd730fc2fec05ce084bf39e87d891c20f897
Yulia Afanasenko <yulia.a.a@outlook.com>
2016-05-23 10:43:18 +0300

    Add readme file

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var config = {}
 
-config.host = process.env.HOST || "***";
-config.authKey = process.env.AUTH_KEY || "***";
+config.host = process.env.HOST || "CONFIGHOSTPARAM";
+config.authKey = process.env.AUTH_KEY || "CONFIGAUTHKEYPARAM";
 config.databaseId = "ratingapplicantsdb";
 config.collectionId = "specializations";
 
